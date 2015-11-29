2c2c1dfb249ad2b9fcf666df216efd0979cd82da
Patrick Spieler <stapelzeiger@gmail.com>
2015-11-29 03:34:13 +0100

    better message forwarder

--- zmqmsgbus/tools/forwarder.py
+++ zmqmsgbus/tools/forwarder.py
@@ -1,14 +1,30 @@
 import zmq
+import argparse
 
-context = zmq.Context()
 
-in_sock = context.socket(zmq.SUB)
-in_sock.bind("tcp://*:13370")
-in_sock.bind("ipc://ipc/sink")
-in_sock.setsockopt(zmq.SUBSCRIBE, b'')
+def forward(from_list, to_list):
+    context = zmq.Context()
 
-out_sock = context.socket(zmq.PUB)
-out_sock.bind("tcp://*:13371")
-out_sock.bind("ipc://ipc/source")
+    in_sock = context.socket(zmq.SUB)
+    in_sock.bind("tcp://*:13370")
+    in_sock.bind("ipc://ipc/sink")
+    in_sock.setsockopt(zmq.SUBSCRIBE, b'')
 
-zmq.device(zmq.FORWARDER, in_sock, out_sock)
+    out_sock = context.socket(zmq.PUB)
+    out_sock.bind("tcp://*:13371")
+    out_sock.bind("ipc://ipc/source")
+
+    zmq.device(zmq.FORWARDER, in_sock, out_sock)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Forward ZeroMQ messages')
+    parser.add_argument('--from', dest='from_addr', nargs='*',
+                        default=['ipc://ipc/sink'])
+    parser.add_argument('--to', dest='to_addr', nargs='*',
+                        default=['ipc://ipc/source'])
+    args = parser.parse_args()
+
+    print('forwarding from {}'.format(args.from_addr))
+    print('to {}'.format(args.to_addr))
+    forward(args.from_addr, args.to_addr)
